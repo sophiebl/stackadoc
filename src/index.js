a0a6b1046a8d274417deb4505b3ce7d6ed0547ca
sophiebl <sophieboulaaouli@gmail.com>
2020-06-11 16:18:59 +0200

    stackadoc

--- src/index.js
+++ src/index.js
@@ -1,17 +1,57 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
+import axios from "axios";
+import "./style.css";
+import logo from "./logo.png";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const App = () => {
+  const url = "http://dummy.restapiexample.com/api/v1/employees";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    var config = {
+        headers: {'Access-Control-Allow-Origin': '*'}
+    };
+    axios
+      .get(url, config)
+      .then(json => {
+        setData(json.data.data)
+      })
+      .catch(e => {
+        console.log("error", e);
+      });
+  }, []);
+
+  const renderTable = () => {
+    return data.map(employee => {
+      return (
+        <tr key={employee.id}>
+          <td>{employee.employee_name}</td>
+          <td>{employee.employee_salary}</td>
+          <td className="al-right">{employee.employee_age}</td>
+        </tr>
+      );
+    });
+  };
+
+  return (
+    <div className="App">
+      <header>
+        <img src={logo} alt="logo" />
+      </header>
+      <table id="users" className="bg-table">
+        <thead>
+          <tr>
+            <th className="name">Name</th>
+            <th className="salary">Salary</th>
+            <th className="age">Age</th>
+          </tr>
+        </thead>
+        <tbody>{renderTable()}</tbody>
+      </table> 
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
